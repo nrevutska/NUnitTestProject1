bd66dae24a512be9cd874e441c43e1dc14f5c1ea
nrevutska <46501186+nrevutska@users.noreply.github.com>
2020-08-08 14:24:11 +0300

    Add files via upload

---
+++ TestStartup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using ProductsWithRouting;
+
+namespace ProdutcsWithRoutingTests
+{
+    public class TestStartup : Startup
+    {
+        public TestStartup(IConfiguration configuration) : base(configuration)
+        {
+        }
+    }
+}
